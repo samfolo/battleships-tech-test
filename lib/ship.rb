3b64353b38481f1defe944faf06f5c25d41398fd
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2020-01-29 21:36:22 +0000

    feat: ship generates its coordinates for B8 location

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,9 +1,12 @@
 class Ship
-  def initialize coordinates
-    @coordinates = coordinates
+  def initialize coordinate
+    @coordinate = coordinate
   end
 
   def location
-    [[0, 0], [1, 0], [2, 0], [3, 0]]
+    case @coordinate
+    when 'A1' then [[0, 0], [1, 0], [2, 0], [3, 0]]
+    when 'B8' then [[1, 7], [2, 7], [3, 7], [4, 7]]
+    end
   end
 end
--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -5,4 +5,9 @@ RSpec.describe Ship do
     test_ship = Ship.new('A1')
     expect(test_ship.location).to eq [[0, 0], [1, 0], [2, 0], [3, 0]]
   end
+
+  it 'holds the coordinates [1, 7], [2, 7], [3, 7] and [4, 7] when passed B8' do
+    test_ship = Ship.new('B8')
+    expect(test_ship.location).to eq [[1, 7], [2, 7], [3, 7], [4, 7]]
+  end
 end
