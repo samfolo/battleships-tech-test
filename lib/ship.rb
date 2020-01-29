ddbe085dfc5a513fbd51f40185220e7ee340f45e
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2020-01-29 21:44:02 +0000

    feat: ship generates its coordinates for F10 location

--- lib/ship.rb
+++ lib/ship.rb
@@ -7,6 +7,8 @@ class Ship
     case @coordinate
     when 'A1' then [[0, 0], [1, 0], [2, 0], [3, 0]]
     when 'B8' then [[1, 7], [2, 7], [3, 7], [4, 7]]
+    when 'C3' then [[2, 2], [3, 2], [4, 2], [5, 2]]
+    when 'F10' then [[5, 9], [6, 9], [7, 9], [8, 9]]
     end
   end
 end
--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -10,4 +10,14 @@ RSpec.describe Ship do
     test_ship = Ship.new('B8')
     expect(test_ship.location).to eq [[1, 7], [2, 7], [3, 7], [4, 7]]
   end
+
+  it 'holds the coordinates [2, 2], [3, 2], [4, 2] and [5, 2] when passed C3' do
+    test_ship = Ship.new('C3')
+    expect(test_ship.location).to eq [[2, 2], [3, 2], [4, 2], [5, 2]]
+  end
+
+  it 'holds the coordinates [5, 9], [6, 9], [7, 9] and [8, 9] when passed F10' do
+    test_ship = Ship.new('F10')
+    expect(test_ship.location).to eq [[5, 9], [6, 9], [7, 9], [8, 9]]
+  end
 end
