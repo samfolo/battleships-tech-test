f6d3082d6501d76f45b01a848c3bf0140daea5ce
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2020-01-29 20:59:25 +0000

    feat: a user can place a ship at B8

--- lib/board.rb
+++ lib/board.rb
@@ -3,6 +3,7 @@ class Board
     first_nine_lines = (1..9).to_a.map.with_index { |num, i| 
       ship&.location&.first == [0, 0] && i.zero? ? "| x x x x #{render_nodes 6} | #{num} " :
       ship&.location&.first == [6, 3] && i == 3 ? "| #{render_nodes 6} x x x x | #{num} " :
+      ship&.location&.first == [1, 7] && i == 7 ? "| • x x x x #{render_nodes 5} | #{num} " :
       "| #{render_nodes 10} | #{num} "
     }.join("\n")
     tenth_line = "| • • • • • • • • • • | 10"
