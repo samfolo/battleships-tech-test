555392236d78be8f4d990a70f556784ab0b90fcb
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2020-01-29 21:06:22 +0000

    feat: a user can place a ship at E5

--- lib/board.rb
+++ lib/board.rb
@@ -1,9 +1,10 @@
 class Board
   def render ship = nil
     first_nine_lines = (1..9).to_a.map.with_index { |num, i| 
-      ship&.location&.first == [0, 0] && i.zero? ? "| x x x x #{render_nodes 6} | #{num} " :
-      ship&.location&.first == [6, 3] && i == 3 ? "| #{render_nodes 6} x x x x | #{num} " :
-      ship&.location&.first == [1, 7] && i == 7 ? "| • x x x x #{render_nodes 5} | #{num} " :
+      ship&.location&.first == [0, 0] && i.zero? ? "| x x x x #{render_nodes 6} | #{num} "         :
+      ship&.location&.first == [6, 3] && i == 3  ? "| #{render_nodes 6} x x x x | #{num} "         :
+      ship&.location&.first == [1, 7] && i == 7  ? "| • x x x x #{render_nodes 5} | #{num} "       :
+      ship&.location&.first == [4, 4] && i == 4  ? "| • • • • x x x x #{render_nodes 2} | #{num} " :
       "| #{render_nodes 10} | #{num} "
     }.join("\n")
     tenth_line = "| • • • • • • • • • • | 10"
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Board do
   let(:test_ship_one) { double :ship, location: [[0, 0], [1, 0], [2, 0], [3, 0]] }
   let(:test_ship_two) { double :ship, location: [[6, 3], [7, 3], [8, 3], [9, 3]] }
   let(:test_ship_three) { double :ship, location: [[1, 7], [2, 7], [3, 7], [4, 7]] }
+  let(:test_ship_four) { double :ship, location: [[4, 4], [5, 4], [6, 4], [7, 4]] }
   let(:empty_board) {
     l1 =  "  A B C D E F G H I J     "
     l2 =  "+ - - - - - - - - - - +   "
@@ -77,21 +78,43 @@ RSpec.describe Board do
     [l1, l2, l3, l4, l5, l6, l7, l8, l9, l10, l11, l12, l13].join("\n")
   }
 
+  let(:test_board_four) {
+    l1 =  "  A B C D E F G H I J     "
+    l2 =  "+ - - - - - - - - - - +   "
+    l3 =  "| • • • • • • • • • • | 1 "
+    l4 =  "| • • • • • • • • • • | 2 "
+    l5 =  "| • • • • • • • • • • | 3 "
+    l6 =  "| • • • • • • • • • • | 4 "
+    l7 =  "| • • • • x x x x • • | 5 "
+    l8 =  "| • • • • • • • • • • | 6 "
+    l9 =  "| • • • • • • • • • • | 7 "
+    l10 = "| • • • • • • • • • • | 8 "
+    l11 = "| • • • • • • • • • • | 9 "
+    l12 = "| • • • • • • • • • • | 10"
+    l13 = "+ - - - - - - - - - - +   "
+    
+    [l1, l2, l3, l4, l5, l6, l7, l8, l9, l10, l11, l12, l13].join("\n")
+  }
+
   it 'at first renders itself with no ships' do
     expect(subject.render).to eq empty_board
   end
 
   describe '#render' do
-    it 'renders a single ship passed as an argument by its location' do
+    it 'renders a single ship passed as an argument by its coordinates' do
       expect(subject.render test_ship_one).to eq test_board_one
     end
 
-    it 'renders a different ship passed as an argument by its location' do
+    it 'renders a different ship passed as an argument by its coordinates' do
       expect(subject.render test_ship_two).to eq test_board_two
     end
 
-    it 'renders another separate ship passed as an argument by its location' do
+    it 'renders another separate ship passed as an argument by its coordinates' do
       expect(subject.render test_ship_three).to eq test_board_three
+    end
+
+    it 'renders another separate ship passed as an argument by its coordinates' do
+      expect(subject.render test_ship_four).to eq test_board_four
     end
   end 
 end
