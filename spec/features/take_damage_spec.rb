038e1c61176a62e716f27f115968b744cb8cf5d5
Sam Folorunsho <s.folorunsho@hotmail.co.uk>
2020-01-30 17:48:27 +0000

    feat: players board can be hit at B-F, F-1 and I-8

--- spec/features/take_damage_spec.rb
+++ spec/features/take_damage_spec.rb
@@ -31,4 +31,28 @@ RSpec.describe "a player taking damage", type: :feature do
 
     expect(player_one.render_board).to eq board
   end
+
+  scenario "a player's board is hit at B-7, F-1 and I-8" do
+    l1 =  "  A B C D E F G H I J     "
+    l2 =  "+ - - - - - - - - - - +   "
+    l3 =  "| • • • • • ! • • • x | 1 "
+    l4 =  "| • x x x x • • • • x | 2 "
+    l5 =  "| • • • • • • • • • x | 3 "
+    l6 =  "| • • • • • x • • • x | 4 "
+    l7 =  "| • • • • • x • • • • | 5 "
+    l8 =  "| x x x x • x • • • • | 6 "
+    l9 =  "| • ! • • • x • • • • | 7 "
+    l10 = "| • • • • • • • • ! • | 8 "
+    l11 = "| • x x x x • • • • • | 9 "
+    l12 = "| • • • • • • x x x x | 10"
+    l13 = "+ - - - - - - - - - - +   "
+
+    board = [l1, l2, l3, l4, l5, l6, l7, l8, l9, l10, l11, l12, l13].join("\n")
+
+    player_one.shot_at('B7')
+    player_one.shot_at('F1')
+    player_one.shot_at('I8')
+
+    expect(player_one.render_board).to eq board
+  end
 end
